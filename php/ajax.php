2737360c6667dca6af056526e2a25d7b433558ed
Bill Altermatt <altermattw@hanover.edu>
2016-09-29 22:22:24 -0400

    Added email notification to reviewer when IRB reviewer has been changed.

--- php/ajax.php
+++ php/ajax.php
@@ -1,9 +1,9 @@
-<?php	
+<?php
 	include('SQLutils.php');
-	if(!isLoggedIn()) {		// have to be logged in for request to be processed.		
-		echo '<p>Error: You are not logged in</p>';		
+	if(!isLoggedIn()) {		// have to be logged in for request to be processed.
+		echo '<p>Error: You are not logged in</p>';
 		die();
-	} else {	
+	} else {
 
 		// getAppRecord
 		if(isset($_POST['getAppRecord']) && isset($_POST['studyNumber'])) {
@@ -20,7 +20,7 @@
 		// returns current status for a particular study
 		if(isset($_POST['getStatus']) && isset($_POST['studyNumber'])) {
 			$mysqli = myConnect();
-			$studyNumber = mysqli_escape_string($mysqli,$_POST['studyNumber']);			
+			$studyNumber = mysqli_escape_string($mysqli,$_POST['studyNumber']);
 			$results = computeCurrentStatus($mysqli,$studyNumber);
 			if($results) {
 				echo json_encode($results);
@@ -42,7 +42,7 @@
 			}
 			echo $reviewer;
 		}
-	
+
 		if(isset($_POST['updateIRB']) && isset($_POST['studyNumber']) && isset($_POST['IRBname']) && isset($_POST['IRBemail'])) {
 			$mysql = myConnect();
 			$email = mysqli_escape_string($mysql,$_POST["IRBemail"]);
@@ -50,15 +50,19 @@
 			$studyNumber = mysqli_escape_string($mysql,$_POST["studyNumber"]);
 			$query = 'UPDATE userapps SET email="'.$email.'", name="'.$name.'" WHERE studyNumber="'.$studyNumber.'" AND role="IRB";';
 			$WM = getWM();
-			if(!$mysql->query($query)) {				
+			if(!$mysql->query($query)) {
 				$reply = '<p>There was a problem deleting the old IRB name and emails for this study from the userapps database. Please email the IRB webmaster, '.$WM["name"].', at '.$WM["email"].' with this message.</p>';
 				$reply.= '<p>Query: '.$query.'</p>';
 				$reply.= '<p>Error: '.$mysql->error.'</p>';
 				echo $reply;
 			} else {
+				$subj = "Assigned as IRB reviewer to study ".$studyNumber;
+				$IRBwebmaster = getWM();
+				$msg = '<p>You have been assigned as the IRB reviewer for study number '.$studyNumber.'. To check the status of this application, log in to <a href="http://irb.hanover.edu">irb.hanover.edu</a> and click on the &ldquo;Go to Administrator Page&rdquo; button or click on your name in the menu bar and select &ldquo;Administrator Dashboard.&rdquo; After the study has been submitted for IRB review, you will be able to suggest revisions or approve the study. If you have questions, please contact '.$IRBwebmaster["name"].' at '.$IRBwebmaster["email"].'.';
+				mailMessage($email,$subj,$msg);
 				echo 1;
-			}								
+			}
 		}
 	}
-		
+
 ?>
